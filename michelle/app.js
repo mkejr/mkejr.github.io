b5ed88da3e0bc8f22214b211d2dea51e9db8343c
Michelle Kejr <michellekejr@michelles-mbp.lan>
2019-05-02 21:26:23 -0500

    initial commit

---
+++ michelle/app.js
@@ -0,0 +1,9 @@
+// (function() {
+// document.getElementById('output').innerHTML = "put different text";
+// document.getElementById('image').innerHTML= "some image";
+// console.log('what does this do?');
+// console.log('frustration');
+// });
+// (function) {
+    // document.getElementById('image').innerHTML = "insert image here";
+// }
---
+++ michelle/test_script.py
@@ -0,0 +1,7 @@
+a=1
+b=3
+
+def math(x,y):
+    return x+y
+
+print(math(a,b))
